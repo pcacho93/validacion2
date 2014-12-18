0d675d9d9fe9f43a4db3b976b366a8f9d5b72948
pcacho <pcacho93@gmail.com>
2014-12-18 12:56:56 +0100

    ejericio4

--- Gruntfile.js
+++ Gruntfile.js
@@ -190,7 +190,7 @@ module.exports = function (grunt) {
       options: {
         dest: '<%= config.dist %>'
       },
-      html: '<%= config.app %>/ejercicio[1,2,3].html'
+      html: '<%= config.app %>/ejercicio[1,2,3,4].html'
     },
 
     // Performs rewrites based on rev and the useminPrepare configuration
---
+++ app/scripts/ejercicio4.js
@@ -0,0 +1,24 @@
+ $("#formulario").validate(
+	 {
+		 rules: {
+		   nombre: {
+			  required: true,
+			  minlength:2,
+			  remote: "php/users.php"
+			  },
+			  email: {
+			   required: true,
+			   remote"php/emails.php"
+			  },
+			  comentarios: "required",
+			  cuenta: {
+				  iban:true,
+				  required: true
+			  },
+			  cuenta2: {
+				 required: true,
+				 cuentabanco: true
+			  }
+			 }
+       }
+ );
